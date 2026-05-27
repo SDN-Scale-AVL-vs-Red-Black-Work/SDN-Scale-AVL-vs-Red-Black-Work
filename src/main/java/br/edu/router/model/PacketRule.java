3cf9375299c469bfeeec980d0c3b34ded38d0953
Joaoemdsn <joao_evangelista.neto@somosicev.com>
2026-05-27 18:28:16 -0300

    Implementação da Classe PacketRule, onde vamos representar os dados que serão armazenados nas árvores.

--- src/main/java/br/edu/router/model/PacketRule.java
+++ src/main/java/br/edu/router/model/PacketRule.java
@@ -1,4 +1,47 @@
 package br.edu.router.model;
 
-public class PacketRule {
+public class PacketRule implements Comparable<PacketRule> {
+
+    private final int id;
+    private final String sourceIp;
+    private final String destinationIp;
+    private final int priority;
+
+    public PacketRule(int id, String sourceIp, String destinationIp, int priority) {
+        this.id = id;
+        this.sourceIp = sourceIp;
+        this.destinationIp = destinationIp;
+        this.priority = priority;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getSourceIp() {
+        return sourceIp;
+    }
+
+    public String getDestinationIp() {
+        return destinationIp;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    @Override
+    public int compareTo(PacketRule other) {
+        return Integer.compare(this.id, other.id);
+    }
+
+    @Override
+    public String toString() {
+        return "PacketRule{" +
+                "id=" + id +
+                ", sourceIp='" + sourceIp + '\'' +
+                ", destinationIp='" + destinationIp + '\'' +
+                ", priority=" + priority +
+                '}';
+    }
 }
