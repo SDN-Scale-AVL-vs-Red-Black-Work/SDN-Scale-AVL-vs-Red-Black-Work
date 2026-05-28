caa2b107b4ae3e9cce6d57753d471b05e35cd48f
Joaoemdsn <joao_evangelista.neto@somosicev.com>
2026-05-27 22:48:57 -0300

    feat: Define as interfaces RouterTree e implementa as classes routertree das árvores AVL e RedBlack

--- src/main/java/br/edu/router/tree/RouterTree.java
+++ src/main/java/br/edu/router/tree/RouterTree.java
@@ -1,5 +1,18 @@
 package br.edu.router.tree;
 
-public class RouterTree {
-    
+import br.edu.router.model.PacketRule;
+
+public interface RouterTree {
+
+    void insert(PacketRule rule);
+
+    boolean search(int id);
+
+    void delete(int id);
+
+    int getRotationCount();
+
+    int size();
+
+    int height();
 }
--- src/main/java/br/edu/router/tree/redblack/RedBlackRouterTree.java
+++ src/main/java/br/edu/router/tree/redblack/RedBlackRouterTree.java
@@ -1,6 +1,35 @@
 package br.edu.router.tree.redblack;
 
+import br.edu.router.model.PacketRule;
 import br.edu.router.tree.RouterTree;
 
-public class RedBlackRouterTree extends RouterTree {
+public class RedBlackRouterTree implements RouterTree {
+@Override
+public void insert(PacketRule rule) {
 }
+
+@Override
+public boolean search(int id) {
+    return false;
+}
+
+@Override
+public void delete(int id) {
+}
+
+@Override
+public int getRotationCount() {
+    return 0;
+}
+
+@Override
+public int size() {
+    return 0;
+}
+
+@Override
+public int height() {
+    return 0;
+}
+}
+
