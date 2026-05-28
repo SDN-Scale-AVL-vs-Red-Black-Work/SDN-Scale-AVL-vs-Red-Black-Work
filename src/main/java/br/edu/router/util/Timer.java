c31e839fc6ef0a62549fea33af6c920a1a04d805
Arthur-Hagel <arthurhagelporfiriomendes@gmail.com>
2026-05-28 11:32:19 -0300

    Timer implementado e DataGenerator corrigido com IDs ordenados

--- src/main/java/br/edu/router/util/Timer.java
+++ src/main/java/br/edu/router/util/Timer.java
@@ -1,4 +1,16 @@
 package br.edu.router.util;
 
 public class Timer {
+    private long startTime;
+    private long endTime;
+
+    public void start() {
+        this.startTime = System.nanoTime();
+    }
+    public void stop() {
+        this.endTime = System.nanoTime();
+    }
+    public long getElapsedTimeInNanoseconds() {
+        return this.endTime - this.startTime;
+    }
 }
